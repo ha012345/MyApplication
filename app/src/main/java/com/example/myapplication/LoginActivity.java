3b5b3cdf567721f3f1e9ee4027b2f921b365b259
jljl73 <--jljl73@naver.com>
2019-10-31 15:02:21 +0900

    user 정보 db추가

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -14,6 +14,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+//
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Pattern;
 
@@ -23,6 +26,12 @@ public class LoginActivity extends AppCompatActivity {
 
     // 파이어베이스 인증 객체 생성
     private FirebaseAuth firebaseAuth;
+
+    // DB
+    private FirebaseDatabase firebaseDatabase;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("user");
+
 
     // 이메일과 비밀번호
     private EditText editTextEmail;
@@ -98,10 +107,15 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // 회원가입 성공
                             Toast.makeText(LoginActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
+                            UserModel userModel = new UserModel();
+                            userModel.UserEmail = editTextEmail.getText().toString();
+                            String uid = task.getResult().getUser().getUid();
+                            FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
                         } else {
                             // 회원가입 실패
                             Toast.makeText(LoginActivity.this, R.string.failed_signup, Toast.LENGTH_SHORT).show();
                         }
+
                     }
                 });
     }
---
+++ app/src/main/java/com/example/myapplication/UserModel.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class UserModel {
+    public String UserEmail;
+
+}
