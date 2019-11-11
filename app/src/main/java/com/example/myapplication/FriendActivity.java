3b2c0d109be7c17974b03bc7e07dae9ea412c49b
jljl73 <--jljl73@naver.com>
2019-11-11 17:49:43 +0900

    Email 중복체크와 사용자체크 추가

--- app/src/main/java/com/example/myapplication/FriendActivity.java
+++ app/src/main/java/com/example/myapplication/FriendActivity.java
@@ -32,13 +32,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 import androidx.navigation.NavController;
-        import androidx.navigation.Navigation;
-        import androidx.navigation.ui.AppBarConfiguration;
-        import androidx.navigation.ui.NavigationUI;
-        import androidx.recyclerview.widget.LinearLayoutManager;
-        import androidx.recyclerview.widget.RecyclerView;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class FriendActivity extends AppCompatActivity {
 
@@ -46,6 +46,8 @@ public class FriendActivity extends AppCompatActivity {
     private MainAdapter mainAdapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
+
+    private String target_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,15 +68,38 @@ public class FriendActivity extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String nickname = et_search_nickname.getText().toString();
-                add_frined(nickname);
-                MainData mainData = new MainData(R.mipmap.ic_launcher, nickname, "아마도 이름");
-                arrayList.add(mainData);
-                mainAdapter.notifyDataSetChanged();
+                target_email = et_search_nickname.getText().toString();
+
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                final DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("User").child(user.getUid()).child("friends");
+
+                ValueEventListener eventListener = new ValueEventListener() {
+                    @Override
+                    // 데이터 돌아다니면서 하나하나 확인
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds : dataSnapshot.getChildren()){
+                            String email = ds.child("friend_email").getValue().toString();
+                            if(email.equals(target_email)){
+                                Toast.makeText(FriendActivity.this, "이미 등록한 사용자 입니다", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+
+                        add_friend(target_email);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                };
+                db.addListenerForSingleValueEvent(eventListener);
+
             }
         });
     }
 
+    // 처음 시작할때 띄우기
     @Override
     protected void onStart() {
         super.onStart();
@@ -85,8 +110,9 @@ public class FriendActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
-                    String nickname = ds.getValue().toString();
-                    MainData mainData = new MainData(R.mipmap.ic_launcher, nickname, "아마도 이름");
+                    String email = ds.child("friend_email").getValue().toString();
+                    String nickname = ds.child("friend_nickname").getValue().toString();
+                    MainData mainData = new MainData(R.mipmap.ic_launcher, email, nickname);
                     arrayList.add(mainData);
                     mainAdapter.notifyDataSetChanged();
                 }
@@ -99,15 +125,50 @@ public class FriendActivity extends AppCompatActivity {
         db.addListenerForSingleValueEvent(eventListener);
     }
 
-    private void add_frined(String nickname){
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("User");
-        if(user != null){
-            String uid = user.getUid();
-            db.child(uid).child("friends").push().setValue(nickname);
-        }
+    //사용자 존재 여부 체크 후 추가
+    private void add_friend(final String email){
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("User");
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                final DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("User");
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    String each_email = ds.child("UserEmail").getValue(String.class);
+                    String each_nickname = ds.child("UserNickName").getValue(String.class);
+                    if(each_email.equals(email)){
+                        String uid = user.getUid();
+                        db.child(uid).child("friends").child(ds.getKey()).child("friend_email").setValue(each_email);
+                        db.child(uid).child("friends").child(ds.getKey()).child("friend_nickname").setValue(each_nickname);
+                        add_frined_to_view(email, each_nickname);
+                        return;
+                    }
+                }
+                Toast.makeText(FriendActivity.this, "없는 사용자 입니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        db.addListenerForSingleValueEvent(eventListener);
     }
 
+
+    //친구 리스트 뷰에 추가
+    private void add_frined_to_view(final String email, String nickname){
+        MainData mainData = new MainData(R.mipmap.ic_launcher, email, nickname);
+        arrayList.add(mainData);
+        mainAdapter.notifyDataSetChanged();
+    }
+
+
+    // 뒤로가기
     @Override
     public void onBackPressed() {
         finish();
