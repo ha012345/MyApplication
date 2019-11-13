cd74bbd9338435248d92f8df23698404bf1aa705
kmt0405 <kmt0405@naver.com>
2019-11-14 04:24:20 +0900

    알고리즘 적용 진행중

--- app/src/main/java/com/example/myapplication/RecommendActivity.java
+++ app/src/main/java/com/example/myapplication/RecommendActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,15 +32,21 @@ public class RecommendActivity extends AppCompatActivity {
         mGroupName = (TextView) findViewById(R.id.group);
 
         Intent intent = getIntent();
-        groupkey = (String) intent.getExtras().get("groupkey");
-        groupname = (String) intent.getExtras().get("groupname");
+        //groupkey = (String) intent.getExtras().get("groupkey");
+        //groupname = (String) intent.getExtras().get("groupname");
+        groupkey = "LtZH0gqqTnzJb9TUGqD";
+        groupname = "algtest";
+
         mGroupName.setText(groupkey + " " + groupname);
 
         Button rec_food = (Button)findViewById(R.id.btn_recommend);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("User").child(user.getUid()).child("Food_Rank");
+        DatabaseReference root = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = root.child("User");//.child(user.getUid()).child("Food_Rank");
+        DatabaseReference groupRef = root.child("group");
+
+        /*
         final int[] max_value = {0};
-        final String[] menu = {""};
         final String[] member = {""};
 
         ValueEventListener eventListener = new ValueEventListener() {
@@ -57,14 +66,78 @@ public class RecommendActivity extends AppCompatActivity {
 
             }
         };
-        ref.addListenerForSingleValueEvent(eventListener);
+        */
+        /// make member e-mail list
+        final List<String> userInGroup = new ArrayList<>();
+        groupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange (@NonNull DataSnapshot dataSnapshot){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String email = ds.getValue().toString();
+
+                    userInGroup.add(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        /// calculate menu score
+
+        final String[] menu = {"Korean", "Snack", "asian", "chicken", "china", "curtlet", "dessert", "fast_food", "lunch_box", "pizza", "pork", "soup"};
+        final int[] score = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        final int[] rank = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        ref.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange (@NonNull DataSnapshot dataSnapshot){
+               for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                   for(String s : userInGroup){
+                       if(ds.child("UserEmail").getValue().toString().equals(s)){
+                           int i = 0;
+                           for(String s1 : menu){
+                               int temp = ds.child("Food_Rank").child(s1).getValue().hashCode();
+                               score[i] += temp;
+                               i++;
+                           }
+                       }
+                   }
+               }
+               /*
+               /// for remove same score : random number add(0-9)
+               for(int i = 0; i < 12; i++){
+                   int randomValue = (int) (Math.random()*10);
+                   score[i] += randomValue;
+               }
+
+                */
+           }
+
+           @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+           }
+        });
+
+        /// sort menu
+        for(int i = 0; i < 12; i++){
+            for(int j = i+1; j < 12; j++){
+                if(score[i] < score[j]) {rank[i]++;}
+            }
+        }
+        final String[] menuRank = {""};
+        for(int i = 0; i < 12; i++){
+            menuRank[rank[i]] = menu[rank[i]];
+        }
 
 
         final TextView textView = (TextView)findViewById(R.id.tv_recommend);
         rec_food.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                textView.setText(menu[0]);
+                textView.setText(menuRank[0]);
             }
         });
     }
